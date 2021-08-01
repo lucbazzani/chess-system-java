79b29753fd06c400abf63306adcfe9a99af02389
lucbazzani <lu.bazzani@hotmail.com>
2021-08-01 16:10:41 -0300

    First Class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,14 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Alo mundao");
+		
+		Position pos = new Position(3, 5);
+		
+		System.out.println(pos);
 
 	}
 
